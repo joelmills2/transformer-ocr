7818514afc2521113fe476f3d179967ebb616608
Joel Mills <joelarimills@gmail.com>
2024-04-25 19:05:02 -0400

    updated comments

--- src/detection.py
+++ src/detection.py
@@ -31,8 +31,8 @@ def preprocess_image(image_path):
     """
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) # Apply thresholding (white or black)
-    return thresh
+    image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] # Apply thresholding (white or black)
+    return image
 
 
 def get_detection(image_path):
